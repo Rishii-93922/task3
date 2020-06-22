78fd3239ef51297792188a02ed8f064c7746fc9f
Rishii-93922 <66353652+Rishii-93922@users.noreply.github.com>
2020-06-22 09:14:21 -0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,81 @@
+{
+  "profiles":[
+    {
+      "basics":{
+        "name":"rishitha",
+        "role":"Student",
+        "email":"rishitha406@gmail.com",
+        "mobile":"+91 7660848947",
+        "gender":"female",
+        "address":"vijayawada,Andhra pradesh"
+
+      },
+      "summery":[
+        "I am Fresher in computer science engineering",
+                "But i having knowledge in some technologies like HTML5,CSS3,and Javascript",
+                "Having knowledge on Python, C++,React",
+                "Used different kinds of frameworks for backend i.e Codeigniter,Django and Express",
+                "Used databases like mySql,oracle and firebase"
+
+      ],
+      "education":[
+        {
+                    "degree":"B-Tech (Computer Science and Engineering)",
+                    "institute":"Lakireddy balireddy College of Engineering",
+                    "result":"85%"
+        },
+        {
+                    "degree":"Intermediate in MPC",
+                    "institute":"Sri Chaitanya Junior College",
+                    "result":"90%"
+        },
+        {
+                    "degree":"S.S.C",
+                    "institute":"vidyanikethan",
+                    "result":"80%"
+        }
+
+
+      ]
+    },
+      {
+        "basics":{
+        "name":"Sai",
+        "role":"Student",
+        "email":"saimandava@gmail.com",
+        "mobile":"+91 7993619983",
+        "gender":"female",
+        "address":"gudivada,Andhra pradesh"
+
+      },
+      "summery":[
+        "I am Fresher in computer science engineering",
+                "But i having knowledge in some technologies like HTML5,CSS3,and Javascript",
+                "Having knowledge on Python, C++,React",
+                "Used different kinds of frameworks for backend i.e ,Django and Express",
+                "Used databases like mySql,oracle"
+
+      ],
+      "education":[
+        {
+                    "degree":"B-Tech (Computer Science and Engineering)",
+                    "institute":"Lakireddy balireddy College of Engineering",
+                    "result":"96%"
+        },
+        {
+                    "degree":"Intermediate in MPC",
+                    "institute":"Sri Chaitanya Junior College",
+                    "result":"95%"
+        },
+        {
+                    "degree":"S.S.C",
+                    "institute":" sri Chaitanya school",
+                    "result":"95%"
+        }
+
+
+      ]
+      }
+    
+  ]
+}
